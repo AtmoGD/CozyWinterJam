5e62199a6243014c997bf0a24055e4b8e8776982
Dennis Hawran <56675253+AtmoGD@users.noreply.github.com>
2022-12-02 19:21:29 +0100

    Added Buildings

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,6 +9,8 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float cameraAcceleration = 10f;
     [SerializeField] private float borderThickness = 10f;
     [SerializeField] private float borderDeadZone = 2f;
+    [SerializeField] private Vector2 cameraBoundsMin = new Vector2(0f, 0f);
+    [SerializeField] private Vector2 cameraBoundsMax = new Vector2(100f, 100f);
 
     private Vector2 dir = Vector2.zero;
     private Vector2 targetDir = Vector2.zero;
@@ -24,11 +26,33 @@ public class CameraController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        MoveCamera();
+
+        ClampCameraBetweenBounds();
+    }
+
+    private void MoveCamera()
+    {
         dir = Vector2.Lerp(dir, targetDir, Time.deltaTime * cameraAcceleration);
-
         mainCamera.transform.Translate(dir * cameraMaxSpeed * Time.deltaTime);
-
         targetDir = Vector2.zero;
+    }
+
+    private void CalculateCameraSize(out Vector2 cameraSize)
+    {
+        Vector2 cornerVector = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+        Vector2 centerVector = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        cameraSize = (cornerVector - centerVector) / 2;
+    }
+
+    private void ClampCameraBetweenBounds()
+    {
+        CalculateCameraSize(out Vector2 cameraSize);
+
+        Vector3 pos = mainCamera.transform.position;
+        pos.x = Mathf.Clamp(pos.x, cameraBoundsMin.x + cameraSize.x, cameraBoundsMax.x - cameraSize.x);
+        pos.y = Mathf.Clamp(pos.y, cameraBoundsMin.y + cameraSize.y, cameraBoundsMax.y - cameraSize.y);
+        mainCamera.transform.position = pos;
     }
 
     public void MoveCamera(Vector2 _mousePosition)
@@ -47,5 +71,11 @@ public class CameraController : MonoBehaviour
 
         if (dir != Vector2.zero && dir.magnitude > 1)
             targetDir.Normalize();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(new Vector3(cameraBoundsMin.x + (cameraBoundsMax.x - cameraBoundsMin.x) / 2, cameraBoundsMin.y + (cameraBoundsMax.y - cameraBoundsMin.y) / 2, 0f), new Vector3(cameraBoundsMax.x - cameraBoundsMin.x, cameraBoundsMax.y - cameraBoundsMin.y, 0f));
     }
 }
